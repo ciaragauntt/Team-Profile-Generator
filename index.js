ce08c8e007cf05134d9d68c786ade75d1c634830
Ciara Gauntt <ci.gauntt@gmail.com>
2022-10-07 11:18:43 -0500

    changed a few things in index.js

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 const htmlCreate = require('./src/htmlCreate');
-
-//connect to specific team javascript
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const Team = [];
+//created array for all information to fit into to create html page
+const teamGenerator = [];
 
+// prompting for manager input 
 const addManager = () => {
     return inquirer.prompt ([
         {
-            //manager name
             type: 'input',
             name: 'name',
             message: 'Please input manager name',
-            validate: name => {
-                if (name) {
-                    return true;
-                } else {
-                    console.log("Managers name required!");
+            validate: managerName => {
+                if (!managerName) {
+                    console.log("Name Required!");
                     return false;
+                } else {
+                    return true;
                 }
             }
         },
         {
-            //manager id
             type: 'input',
             name: 'id',
             message: 'Please input manager id',
-            validate: name => {
-                if (isNaN(name)) {
+            validate: managerId => {
+                if (isNaN(managerId) || managerId.trim() === ' ') {
                     console.log('Manager id required!');
                     return false;
                 } else {
@@ -41,27 +38,25 @@ const addManager = () => {
             }
         },
         {
-            //manager email
             type: 'input',
             name: 'email',
             message: 'Please input managers email',
-            validate: email => {
-                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-                if(valid) {
-                    return true;
-                } else {
-                    console.log('Enter a correct email');
+            validate: managerEmail => {
+                const valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                if (!valid.test(managerEmail)) {
+                    console.log("Email required!");
                     return false;
+                } else {
+                    return true;
                 }
             }
         },
         {
-            // manager office number
             type: 'input',
             name: 'officeNumber',
             message: 'Please input managers office number',
-            validate: name => {
-                if (isNaN (name)) {
+            validate: officeNumber => {
+                if(!officeNumber) {
                     console.log('Office number required!');
                     return false
                 } else {
@@ -70,47 +65,44 @@ const addManager = () => {
             }
         }
     ])
+// adding manager to the array to be pushed to the htmlcreate
     .then (managerInput => {
-        const { name, id, email, officeNumber } = managerInput;
-        const manager = new Manager (name, id, email, officeNumber);
+        let { name, id, email, officeNumber } = managerInput;
+        let manager = new Manager (name, id, email, officeNumber);
 
-        Team.push(manager);
-        console.log(manager);
+        teamGenerator.push(manager);
     })
 };
 
+// prompting employee input
 const addEmployee = () => {
-    console.log('*****ADDING EMPLOYEE TO TEAM*****');
 
     return inquirer.prompt ([
         {
-            //emplyee role
             type: 'list',
             name: 'role',
             message: 'What is the employees role?',
             choices: ['Engineer', 'Intern']
         },
         {
-            // employee name
             type: 'input',
             name: 'name',
             message: 'What is the Employees name?',
-            validate: name => {
-                if (name) {
-                    return true;
-                } else {
-                    console.log('Employee name required!');
+            validate: employeeName => {
+                if (!employeeName) {
+                    console.log("Employee name required!");
                     return false;
+                } else {
+                    return true;
                 }
             }
         },
         {
-            // employee id
             type: 'input',
             name: 'id',
             message: 'Enter employee id',
-            validate: name => {
-                if (isNaN(name)) {
+            validate: employeeId => {
+                if (isNaN(employeeId) || employeeId.trim() === '') {
                     console.log('Employee Id required!');
                     return false;
                 } else {
@@ -120,22 +112,20 @@ const addEmployee = () => {
 
         },
         {
-            // employee email
             type: 'input',
             name: 'email',
             message: 'Enter employee email',
-            validate: email => {
-                valid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-                if (valid) {
-                    return true;
-                } else {
-                    console.log('Email required!')
+            validate: employeeEmail => {
+                const valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                if (!valid.test(employeeEmail)) {
+                    console.log("Email required!");
                     return false;
+                } else {
+                    return true;
                 }
             }
         },
         {
-            // employee github
             type: 'input',
             name: 'github',
             message: 'Enter employees github username',
@@ -150,7 +140,6 @@ const addEmployee = () => {
             }
         },
         {
-            // intern school
             type: 'input',
             name: 'school',
             message: 'Enter interns school',
@@ -165,36 +154,35 @@ const addEmployee = () => {
             }
         },
         {
-            // confirming adding the employee/inten
             type: 'confirm',
             name: 'confirmAdd',
             message: 'Would you like to add another team member?',
             default: false
         }
     ])
+// adding employee to the array to be added to the htmlcreate
     .then (employeeData => {
-        let {name, id, email, role, github, school, confirmAdd} = employeeData;
-        let employee;
+       let {name, id, email, role, github, school, confirmAdd} = employeeData
+       let employee;
 
         if (role === "Engineer") {
-            employee = new Engineer (name, id, email, github);
-            console.log(employee);
+            employee = new Engineer(name, id, email, github)
+            
         } else if (role === "Intern") {
-            employee = new Intern (name, id, email, school);
-            console.log(employee);
+            employee  = new Intern(name, id, email, school);
+           
         }
-        Team.push(employee);
+        teamGenerator.push(employee);
 
         if (confirmAdd) {
-            return addEmployee(Team);
+            return addEmployee(teamGenerator);
         } else {
-            return Team;
+            return teamGenerator;
         }
     })
 };
 
-// generate html page
-
+// using fs to write the file and add it to the index.html
 const writeFile = data => {
     fs.writeFile('./dist/index.html', data, err => {
         if(err) {
@@ -206,10 +194,12 @@ const writeFile = data => {
     })
 };
 
+
+
 addManager()
     .then(addEmployee)
-    .then (Team => {
-        return htmlCreate(Team);
+    .then (teamGenerator => {
+        return htmlCreate(teamGenerator);
     })
     .then(HTMLpage => {
         return writeFile(HTMLpage);
